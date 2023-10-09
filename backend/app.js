58e901edfe287216948f33884cef9ee5e4a2369a
Youssef Mostafa <88552711+Youssef-Hossam-Mostafa@users.noreply.github.com>
2023-10-09 15:05:17 +0300

    quickFix: resolve CORS error tested on GET req only

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,12 @@ const prescriptionRouter = require('./routes/prescriptionRoutes');
 const packageRouter = require('./routes/packageRoutes');
 const Prescription = require('./models/prescriptionModel');
 const patientController = require('./controllers/patientController');
-const { addPackage, getPackages, editPackage, deletePackage } = require('./controllers/packageController');
-
+const {
+  addPackage,
+  getPackages,
+  editPackage,
+  deletePackage,
+} = require('./controllers/packageController');
 
 // const patientController = require('./controllers/patientController');
 // const adminController = require('./controllers/adminController');
@@ -23,12 +27,23 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use('/admins', adminRouter);
 app.use('/doctors', doctorRouter);
 app.use('/patients', patientRouter);
 app.use('/prescriptions', prescriptionRouter);
-app.use('/packages',packageRouter);
-
+app.use('/packages', packageRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`));
