0df3627f49693c362017d50906844cdac606c32e
mariaamashraaf <127663272+mariaamashraaf@users.noreply.github.com>
2023-10-11 03:23:37 +0300

    Feature : added doctor homepage

---
+++ frontend/src/doctor/pages/DoctorHome.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import DataTable from "../../../shared/components/DataTable/DataTable";
+
+const DoctorHome = () => {
+    const [users, setUsers] = useState([]);
+    const [requests, setRequests] = useState([]);
+    const [isUserTab, setUserTab] = useState(true);
+    const [showRequest, setShowRequest] = useState(false);
+    const [showUser, setShowUser] = useState(false);
+    const [showAdminForm, setShowAdminForm] = useState(false);
+    const [selectedRow, setSelectedRow] = useState({});
+
+    useEffect(() => {
+        fetchMyPatients();
+        fetchUpcomingAppointments();
+        fetchMyInfo();
+    }, [])
+
+
+    const upcomingCols = [ 
+        { field: "username", headerName: "Username" },
+        { field: "name", headerName: "Name" },
+        { field: "email", headerName: "Email" },
+        { field: "status", headerName: "Status" },
+    ]
+
+    const patientCols = [
+        { field: "username", headerName: "Username" },
+        { field: "name", headerName: "Name" },
+        { field: "mobileNumber", headerName: "Mobile Number" },
+    ]
+
+    const infoCols = [  //khaliha text
+        { field: "username", headerName: "Username" },
+        { field: "name", headerName: "Name" },
+        { field: "mobileNumber", headerName: "Mobile Number" },
+        { field: "role", headerName: "Role" },
+    ]
+
+    const fetchMyPatients = () => {
+        fetch("http://localhost:3000/doctors/:doctorId/patients").then(async (response) => {
+            const json = await response.json();
+            const patientsJson = json.data.patients;
+            setUsers((val) => [...val, ...patientsJson.map((patient) => {
+                return {
+                    id: patient["_id"],
+                    username: patient["username"],
+                    name: patient["name"],
+                    email: patient['email'],
+                    dateOfBirth: patient['dateOfBirth'],
+                    gender: patient['gender'],
+                    mobileNumber: patient["mobileNumber"],
+                    emergencyContact: patient['emergencyContact'],
+                    doctor: patient['doctor'],
+                    familyMembers: patient['familyMembers'],
+                }
+            })]);
+        });
+    }
+
+
+
+
+}
+
+export default DoctorHome;
+
