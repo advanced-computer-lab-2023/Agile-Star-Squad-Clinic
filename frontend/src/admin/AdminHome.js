a88417ef5ddc871d988d4c5e9afc89bef0d913cc
mariiamemad <mariiammemad@gmail.com>
2023-10-11 17:59:52 +0300

    Admin homepage

---
+++ frontend/src/admin/AdminHome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const AdminHome = props => {
+
+  
+    return ( 
+        <div>
+        <h1>Welcome to Admin Home</h1>
+        <button to="/">Manage Users</button>
+        <button to="/">Request List</button>
+        <button to="/">Packages Page</button>
+      </div>
+      );
+  };
+
+  export default AdminHome;
+
+
+
+
+
