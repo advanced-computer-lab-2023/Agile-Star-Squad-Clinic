b990595be58659d00edeb5c94cd8f531d94accca
Lasheen2001 <ahmedlasheen2412@gmail.com>
2023-10-12 00:43:04 +0300

    .

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -4,7 +4,6 @@ const Doctor = require('./doctorModel');
 const Patient = require('./patientModel');
 
 const appointmentSchema = new mongoose.Schema({
-<<<<<<< HEAD
   doctor: {
     type: mongoose.Schema.ObjectId,
     ref: 'Doctor',
@@ -22,34 +21,6 @@ const appointmentSchema = new mongoose.Schema({
     required: [true, 'Please provide a date'],
     default: Date.now,
   },
-=======
-    doctor:
-    {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Doctor',
-        required: [true, 'A doctor must be appointed to a patient.'],
-    },
-
-    patient:
-    {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Patient',
-        required: [true, 'A patient must be appointed to a doctor.'],
-    },
-
-
-    dateOfAppointment: {
-        type: Date,
-        required: [true, 'Please provide a date'],
-        default: Date.now
-    },
-
-    status: {
-        type: String,
-        enum: ["vaccant", "reserved", 'passed'],
-        required: [true, 'Please provide status'],
-    },
->>>>>>> 008e233b9dd69ac6230d2b8544abba90ca0fa4b7
 
   status: {
     type: String,
