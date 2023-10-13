460afddc9ecd187895ce61cc0da12f7c97c85007
Shampoo <kareemelkaderi@gmail.com>
2023-10-13 14:36:16 +0300

    add family member part 1 :D

--- frontend/src/patient/pages/PatientFamily.js
+++ frontend/src/patient/pages/PatientFamily.js
@@ -5,20 +5,21 @@ const PatientFamily = () => {
   const [familyMembers, setFamilyMembers] = useState(false);
   const [error, setError] = useState();
   const [listFamilyMember, setNewFamilyMember] = useState([]);
- 
+  const [isShowMemberForm, setShowMemberForm] = useState(false);
+
 
   useEffect(() => {
     const sendRequest = async () => {
       console.log("SENDING REQUEST")
       setFamilyMembers(true);
-      
+
       try {
         const response = await fetch("http://localhost:3000/patients/6521b46c8e7a4831ac7e6dce/familyMembers");
         console.log("HAS FETCHED")
         const responseData = await response.json();
         setNewFamilyMember(responseData.data.members)
         console.log(responseData.data.members)
-        
+
         if (!response.ok) {
           throw new Error(responseData.message); // Fix the message parameter
         }
@@ -32,13 +33,24 @@ const PatientFamily = () => {
       setFamilyMembers(false);
     };
 
-    sendRequest();
-  }, []);
+    if (!isShowMemberForm) {
+      sendRequest();
+    }
+  }, [isShowMemberForm]);
+
+  const showMemberForm = () => {
+    setShowMemberForm(true);
+  }
+
+  const exitMemberModal = () => {
+    setShowMemberForm(false);
+  }
 
   return (
     <div>
       <h1>My Family Members</h1>
-      <button className="btn btn-primary">Add Family Members</button>
+      <button className="btn btn-primary" onClick={showMemberForm}>Add Family Members</button>
+      {isShowMemberForm && <AddFamilyForm exit={exitMemberModal} />}
 
       {listFamilyMember && <FamilyList items={listFamilyMember} />}
     </div>
