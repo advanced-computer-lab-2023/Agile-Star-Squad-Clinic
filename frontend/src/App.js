a84e5ab243c6fb0d6be461166530ba3efc787e96
Youssef Mostafa <88552711+Youssef-Hossam-Mostafa@users.noreply.github.com>
2023-10-13 17:22:57 +0300

    refactor: clean index & app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
 
 import LandingPage from './shared/components/LandingPage/landingPage';
 import PatientRegisterForm from './shared/components/FormElements/patientRegisterForm';
+import './App.css';
 
-// import {getAllPatients} from '../src/data/controllers/patientController';
 
 function App() {
-  // const patients = getAllPatients();
-  // console.log(patients);
 
   return (
     <div className="App">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,44 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter as Router,
-  Route,
- Routes
-} from 'react-router-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PatientRegisterForm from './shared/components/FormElements/patientRegisterForm';
-import LandingPage from './shared/components/LandingPage/landingPage';
-import ManageUsersPage from './admin/pages/ManageUsers/ManageUsersPage';
-import DoctorHome from './doctor/pages/DoctorHome';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    {/* <div id="backdrop-root"></div>
-    <DoctorHome /> /}
-    {/ <Router>
-      <Routes>
-        <Route path="/" exact>
-          <LandingPage />
-        </Route>
-        <Route path="/patientRegisterForm" exact>
-          <PatientRegisterForm />
-        </Route> /}
-        {/ <Route path="/places/new" exact>
-        <NewPlace />
-      </Route>
-      <Route path="/:userId/places" exact>
-        <UserPlaces /> /
-        / </Route> /}
-        {/ </Routes>
-
-    </Router> */}
-    <App />
-  </>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
