4173a1d38f87b8080c669bb2f9d52fe19582d120
Shampoo <kareemelkaderi@gmail.com>
2023-10-15 05:58:07 +0300

    //////// SPRINT 1 ////////

--- backend/models/patientModel.js
+++ backend/models/patientModel.js
@@ -10,7 +10,7 @@ const patientSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a username.'],
     unique: true,
-    // maxlength: [30, 'A username must have less or equal then 30 characters'],
+    maxlength: [30, 'A username must have less or equal then 30 characters'],
     // minlength: [8, 'A username must have more or equal to 8 characters'],
   },
   name: {
