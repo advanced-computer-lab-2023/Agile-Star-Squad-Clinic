4b69f83d089249eda68a067ad89a0d4616bc9a46
Lasheen2001 <ahmedlasheen2412@gmail.com>
2023-11-10 18:43:29 +0200

    adjusting logo

--- frontend/src/login/pages/login.js
+++ frontend/src/login/pages/login.js
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../images/logo.svg';
 import img from '../images/login-image.png';
-import img2 from '../images/Rectangle1.png'
+import img2 from '../images/Rectangle1.png';
 import '../components/login.css';
+import InputField from '../components/InputField/InputField';
 
 const Login = () => {
   return (
@@ -9,11 +11,21 @@ const Login = () => {
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-7">
-            <img className='sama3a' src={img} alt='login'></img>
+            <div className="logo">
+              <img  src={logo} alt="logo" />
+            </div>
+            <img className="sama3a" src={img} alt="login"></img>
           </div>
 
-          <div class="col-md-5" id='right-col'>
-            <img className='white-box' src={img2} alt='white-box'></img>
+          <div class="col-md-5" id="right-col">
+            <InputField />
+            <InputField />
+            <InputField />
+            <InputField />
+            <InputField />
+            <InputField />
+            <InputField />
+            <InputField />
           </div>
         </div>
       </div>
