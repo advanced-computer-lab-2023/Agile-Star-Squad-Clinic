00a69b3ca2bd60fe07c49c3731012d1b733b156f
Youssef Mostafa <88552711+Youssef-Hossam-Mostafa@users.noreply.github.com>
2023-11-14 02:53:37 +0200

    fix: remove <body>

--- frontend/src/login/pages/ResetPassword.js
+++ frontend/src/login/pages/ResetPassword.js
@@ -12,27 +12,24 @@ const ResetPassword = () => {
   const [tab, setTab] = useState(false);
   const [tab2, setTab2] = useState(false);
   const [tab3, setTab3] = useState(false);
-  const [email , setEmail] = useState('');
+  const [email, setEmail] = useState('');
 
   return (
-    <body>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-5">
-            <div className={styles.logo}>
-              <img src={logo} alt="logo" />
-            </div>
-            <img className={styles.bandage} src={img} alt="login" />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-5">
+          <div className={styles.logo}>
+            <img src={logo} alt="logo" />
           </div>
-          {!tab && (<Component0 setTab={setTab} setEmail2={setEmail} />)}
-          {tab && (!tab2 && (<Component1 setTab2={setTab2} email={email}/>)) }
-          {tab2 && (!tab3 && (<Component2 setTab3={setTab3} email={email}/>))}
-          {tab3 && (<Component3/>)}
-          
+          <img className={styles.bandage} src={img} alt="login" />
         </div>
+        {!tab && <Component0 setTab={setTab} setEmail2={setEmail} />}
+        {tab && !tab2 && <Component1 setTab2={setTab2} email={email} />}
+        {tab2 && !tab3 && <Component2 setTab3={setTab3} email={email} />}
+        {tab3 && <Component3 />}
       </div>
-    </body>
+    </div>
   );
-  };
+};
 
 export default ResetPassword;
