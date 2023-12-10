e27b1a70c21a8d35554763d84bd12a924d9837ee
Shampoo <kareemelkaderi@gmail.com>
2023-12-10 15:53:12 +0200

    gender and relation dropdowns

--- frontend/src/patient/pages/AccountSettings/FamilyCard.js
+++ frontend/src/patient/pages/AccountSettings/FamilyCard.js
@@ -1,6 +1,7 @@
 import { SideCard } from './Account';
 import { useEffect, useState } from 'react';
 import classes from "./FamilyCard.module.css";
+import Select from "react-select";
 import closeImg from '../../../assets/patientAccount/close.png';
 
 const FamilyCard = (props) => {
@@ -50,6 +51,18 @@ const FamilyCard = (props) => {
     }
 
     const getForm = () => {
+      const relationOptions = [
+        {value: "son", label: "Son"},  
+        {value: "daughter", label: "Daughter"},  
+        {value: "wife", label: "Wife"},  
+        {value: "husband", label: "Husband"},  
+      ];
+
+      const genderOptions = [
+        {"value": "male", label:"Male"},
+        {"value": "female", label:"Female"},
+      ];
+      
       const onSubmit = () => {
         // TODO Submit to backend
       }
@@ -59,14 +72,32 @@ const FamilyCard = (props) => {
           <div className={classes.formTitle}>Member Details</div>
           <div className={classes.inputRow}>
             <input value={name} onChange={(e) => setName(e.target.value)} placeholder='Full Name' required/>
-            <input value={nationalId} onChange={(e) => setNationalId(e.target.value)}  placeholder='National ID' required/>
+            <input type='number' value={nationalId} onChange={(e) => setNationalId(e.target.value)}  placeholder='National ID' required/>
           </div>
           <div className={classes.inputRow}>
-            <input value={age} onChange={(e) => setAge(e.target.value)}  placeholder='Age' required/>
-            <input value={gender} onChange={(e) => setGender(e.target.value)}  placeholder='Gender' required/>
+            <div className='d-flex w-100 ms-3 align-items-center justify-content-between'>
+              Gender:
+              <Select
+               options={genderOptions}
+               styles={customStyles}
+               value={gender}
+               isSearchable={false}
+               onChange={(option) => setGender(option)}
+              />
+            </div>
+            <div className='d-flex w-100 mx-3 align-items-center justify-content-between'>
+              Relation:
+              <Select
+               options={relationOptions}
+               styles={customStyles}
+               value={relation}
+               isSearchable={false}
+               onChange={(option) => setRelation(option)}
+              />
+            </div>
           </div>
-          <div className={classes.inputRow}>
-            <input value={relation} onChange={(e) => setRelation(e.target.value)}  placeholder='Relation To Parent' required/>
+          <div className={`${classes.inputRow} w-25`}>
+            <input type='number' value={age} onChange={(e) => setAge(e.target.value)}  placeholder='Age' required/>
           </div>
         </div>
         <div className='mt-4 w-100'>
@@ -75,7 +106,7 @@ const FamilyCard = (props) => {
             <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email'/>
           </div>
           <div className={classes.inputRow}>
-            <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}  placeholder='Phone Number'/>
+            <input type='number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}  placeholder='Phone Number'/>
           </div>
         </div>
         <button type="submit" onClick={onSubmit} className={classes.addButton}>Add Member</button>
@@ -94,3 +125,62 @@ const FamilyCard = (props) => {
   }
 
   export default FamilyCard;
+
+    
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      width: "110px",
+      backgroundColor: 'white',
+      border: 'none',
+      boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.09)",
+      borderRadius: "17px",
+      textAlign: 'start'
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      color: "#193842"
+    }),
+    indicatorSeparator: () => ({}),
+    menu: (provided) => ({
+      ...provided,
+      borderRadius: '20px',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      borderRadius: '14px',
+      fontSize: '14px',
+      fontWeight: state.isFocused ? "500" : "400",
+      color: state.isFocused ? "black" : "#666666",
+      textAlign: "left",
+      backgroundColor: "transparent"
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      fontSize: '12px',
+      fontWeight: "600",
+      color: "#193842"
+    }),
+    valueContainer: (provided) => ({
+      ...provided,
+      backgroundColor: "transparent"
+    }),
+    menuList: (base) => ({
+      ...base,
+      maxHeight: '180px',
+      "::-webkit-scrollbar": {
+        width: "3px",
+        height: "0px",
+      },
+      "::-webkit-scrollbar-track": {
+        background: "transparent"
+      },
+      "::-webkit-scrollbar-thumb": {
+        background: "#888",
+        borderRadius: '3px',
+      },
+      "::-webkit-scrollbar-thumb:hover": {
+        background: "#555"
+      }
+    })
+  };
