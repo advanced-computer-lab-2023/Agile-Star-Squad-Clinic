66379e62de4657b05ff87ae9ab06ed7aaed2a187
Youssef Mostafa <88552711+Youssef-Hossam-Mostafa@users.noreply.github.com>
2023-12-15 17:13:33 +0200

    chore: get patient name in meeting

--- frontend/src/shared/pages/Meeting.js
+++ frontend/src/shared/pages/Meeting.js
@@ -9,178 +9,198 @@ import { authToken, createMeeting } from '../components/Meeting/API';
 import ReactPlayer from 'react-player';
 import axios from 'axios';
 import UserContext from '../../user-store/user-context';
-
-
+import { Link } from 'react-router-dom';
 
 const Meeting = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [meetingId, setMeetingId] = useState(null);
+  const userCtx = useContext(UserContext);
 
-    useEffect(() => {
-        fetchMeeting();
-    }, []);
+  const [isLoading, setIsLoading] = useState(true);
+  const [meetingId, setMeetingId] = useState(null);
+  const [name, setName] = useState(null);
 
-    const fetchMeeting = async () => {
-        try {
-            
-        const meetingResult = await axios.get('http://localhost:3000/meeting', { withCredentials: true })
-            setMeetingId(meetingResult.data.data.meeting.meetingId);
-        } catch (error) {
-            const meetingId = await createMeeting({ token: authToken });
-            await axios.post(
-                'http://localhost:3000/meeting',
-                { meetingId },
-                { withCredentials: true },
-              );
-            setMeetingId(meetingId);
-        }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const meetingResult = await axios.get('http://localhost:3000/meeting', {
+        withCredentials: true,
+      });
+      setMeetingId(meetingResult.data.data.meeting.meetingId);
+    } catch (error) {
+      const meetingId = await createMeeting({ token: authToken });
+      await axios.post(
+        'http://localhost:3000/meeting',
+        { meetingId },
+        { withCredentials: true },
+      );
+      setMeetingId(meetingId);
     }
 
-    useEffect(() => {
-        if (meetingId) {
-            setIsLoading(false);
-        }
-    }, [meetingId]);
+    try {
+      const nameResult = await axios.get(
+        `http://localhost:3000/${userCtx.role}s/${userCtx.userId}`,
+        {
+          withCredentials: true,
+        },
+      );
+      setName(nameResult.data.data.patient.name);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const onMeetingLeave = () => {
-        axios
-          .delete('http://localhost:3000/meeting', { withCredentials: true })
-          .then((res) => {
-            setMeetingId(null);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+  useEffect(() => {
+    if (meetingId && name) {
+      setIsLoading(false);
+    }
+  }, [meetingId, name]);
+
+  const onMeetingLeave = () => {
+    axios
+      .delete('http://localhost:3000/meeting', { withCredentials: true })
+      .then((res) => {
         setMeetingId(null);
-      };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setMeetingId(null);
+  };
 
-    return isLoading ? <div>Loading</div> :
-        <MeetingProvider
-            config={{
-                meetingId,
-                micEnabled: true,
-                webcamEnabled: true,
-                name: "Kimo",
-            }}
-            token={authToken}
-        >
-            <MeetingConsumer>
-                {() => (
-                    <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
-                )}
-            </MeetingConsumer>
-        </MeetingProvider>
-}
+  return isLoading ? (
+    <div>Loading</div>
+  ) : (
+    <MeetingProvider
+      config={{
+        meetingId,
+        micEnabled: true,
+        webcamEnabled: true,
+        name,
+      }}
+      token={authToken}
+    >
+      <MeetingConsumer>
+        {() => (
+          <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
+        )}
+      </MeetingConsumer>
+    </MeetingProvider>
+  );
+};
 
 export default Meeting;
 
 const MeetingView = (props) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const initialized = useRef(false)
-    const { join } = useMeeting();
-    const { participants } = useMeeting({
-      onMeetingJoined: () => {
-        setIsLoading(false);
-      },
-      onMeetingLeft: () => {
-        props.onMeetingLeave();
-      }
-    });
+  const [isLoading, setIsLoading] = useState(true);
+  const initialized = useRef(false);
+  const { join } = useMeeting();
+  const { participants } = useMeeting({
+    onMeetingJoined: () => {
+      setIsLoading(false);
+    },
+    onMeetingLeft: () => {
+      props.onMeetingLeave();
+    },
+  });
 
-    useEffect(() => {
-        if (!initialized.current) {
-            initialized.current = true
-            joinMeeting();
-          }
-    }, []);
-
-    const joinMeeting = () => {
-        setTimeout(() => {
-            join();
-        }, 100);
+  useEffect(() => {
+    if (!initialized.current) {
+      initialized.current = true;
+      joinMeeting();
     }
+  }, []);
 
+  const joinMeeting = () => {
+    setTimeout(() => {
+      join();
+    }, 100);
+  };
 
-
-    return isLoading ? <div>Loading</div> : <div>
-        <Controls />
-        {[...participants.keys()].map((participantId) => (
-            <ParticipantView
-                participantId={participantId}
-                key={participantId}
-            />
-        ))}
-    </div>;
-}
+  return isLoading ? (
+    <div>Loading</div>
+  ) : (
+    <div>
+      <Controls />
+      {[...participants.keys()].map((participantId) => (
+        <ParticipantView participantId={participantId} key={participantId} />
+      ))}
+    </div>
+  );
+};
 
 function Controls() {
-    const { leave, toggleMic, toggleWebcam } = useMeeting();
-    return (
-        <div>
-            <button onClick={() => leave()}>Leave</button>
-            <button onClick={() => toggleMic()}>toggleMic</button>
-            <button onClick={() => toggleWebcam()}>toggleWebcam</button>
-        </div>
-    );
+  const { leave, toggleMic, toggleWebcam } = useMeeting();
+
+  return (
+    <div>
+      <Link to="/">
+        <button onClick={() => leave()}>Leave</button>
+      </Link>
+      <button onClick={() => toggleMic()}>toggleMic</button>
+      <button onClick={() => toggleWebcam()}>toggleWebcam</button>
+    </div>
+  );
 }
 
 function ParticipantView(props) {
-    const micRef = useRef(null);
-    const { webcamStream, micStream, webcamOn, micOn, isLocal, displayName } =
-      useParticipant(props.participantId);
-  
-    const videoStream = useMemo(() => {
-      if (webcamOn && webcamStream) {
+  const micRef = useRef(null);
+  const { webcamStream, micStream, webcamOn, micOn, isLocal, displayName } =
+    useParticipant(props.participantId);
+
+  const videoStream = useMemo(() => {
+    if (webcamOn && webcamStream) {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(webcamStream.track);
+      return mediaStream;
+    }
+  }, [webcamStream, webcamOn]);
+
+  useEffect(() => {
+    if (micRef.current) {
+      if (micOn && micStream) {
         const mediaStream = new MediaStream();
-        mediaStream.addTrack(webcamStream.track);
-        return mediaStream;
+        mediaStream.addTrack(micStream.track);
+
+        micRef.current.srcObject = mediaStream;
+        micRef.current
+          .play()
+          .catch((error) =>
+            console.error('videoElem.current.play() failed', error),
+          );
+      } else {
+        micRef.current.srcObject = null;
       }
-    }, [webcamStream, webcamOn]);
-  
-    useEffect(() => {
-      if (micRef.current) {
-        if (micOn && micStream) {
-          const mediaStream = new MediaStream();
-          mediaStream.addTrack(micStream.track);
-  
-          micRef.current.srcObject = mediaStream;
-          micRef.current
-            .play()
-            .catch((error) =>
-              console.error('videoElem.current.play() failed', error),
-            );
-        } else {
-          micRef.current.srcObject = null;
-        }
-      }
-    }, [micStream, micOn]);
-  
-    return (
-      <div key={props.participantId}>
-        <p>
-          Participant: {displayName} | Webcam: {webcamOn ? 'ON' : 'OFF'} | Mic:{' '}
-          {micOn ? 'ON' : 'OFF'}
-        </p>
-        <audio ref={micRef} autoPlay muted={isLocal} />
-        {webcamOn && (
-          <ReactPlayer
-            //
-            playsinline // very very imp prop
-            pip={false}
-            light={false}
-            controls={false}
-            muted={true}
-            playing={true}
-            //
-            url={videoStream}
-            //
-            height={'200px'}
-            width={'300px'}
-            onError={(err) => {
-              console.log(err, 'participant video error');
-            }}
-          />
-        )}
-      </div>
-    );
-  }
+    }
+  }, [micStream, micOn]);
+
+  return (
+    <div key={props.participantId}>
+      <p>
+        Participant: {displayName} | Webcam: {webcamOn ? 'ON' : 'OFF'} | Mic:{' '}
+        {micOn ? 'ON' : 'OFF'}
+      </p>
+      <audio ref={micRef} autoPlay muted={isLocal} />
+      {webcamOn && (
+        <ReactPlayer
+          //
+          playsinline // very very imp prop
+          pip={false}
+          light={false}
+          controls={false}
+          muted={true}
+          playing={true}
+          //
+          url={videoStream}
+          //
+          height={'200px'}
+          width={'300px'}
+          onError={(err) => {
+            console.log(err, 'participant video error');
+          }}
+        />
+      )}
+    </div>
+  );
+}
