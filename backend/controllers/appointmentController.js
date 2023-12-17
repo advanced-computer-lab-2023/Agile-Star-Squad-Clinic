bb7a192c99eaf9b783c1742cb6f5c2a0c4432995
AhmedMosad0 <ahmed.neinaa00@gmail.com>
2023-12-17 15:55:29 +0200

    chore: create notification upon rescheduling appointment

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -81,7 +81,7 @@ exports.deleteAppointment = catchAsync(async (req, res, next) => {
   const pMessage = `Your appointment with doctor ${doctor.name} at ${req.body.dateOfAppointment} has been canceled`
   const dMessage = `Your appointment with patient ${patient.name} at ${req.body.dateOfAppointment} has been canceled`
 
-  const newNotification = await Notifications.create({ patient: req.body.patient, doctor: req.body.doctor, appoinmentDate: req.body.dateOfAppointment, appointmentStatus: newAppointment.status, patientMessage: pMessage, doctorMessage: dMessage });
+  const newNotification = await Notifications.create({ patient: req.body.patient, doctor: req.body.doctor, appoinmentDate: req.body.dateOfAppointment, appointmentStatus: appointment.status, patientMessage: pMessage, doctorMessage: dMessage });
 
 
   patient.appointments.pull(appointment._id);
@@ -92,6 +92,9 @@ exports.deleteAppointment = catchAsync(async (req, res, next) => {
   await doctor.save();
   patient.notifications.push(newNotification);
   await patient.save();
+
+  await patient.save({ validateBeforeSave: false });
+  await doctor.save({ validateBeforeSave: false });
 
   try {
 
@@ -122,17 +125,61 @@ exports.deleteAppointment = catchAsync(async (req, res, next) => {
 });
 
 exports.updateAppointment = catchAsync(async (req, res, next) => {
+  console.log("DAKHALT")
   const appointment = await Appointment.findByIdAndUpdate(
     req.params.id,
     req.body
   );
+  console.log("ALOO")
+  console.log(appointment)
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      appointment,
-    },
-  });
+
+  const patient = await Patient.findById(appointment.patient);
+  console.log(patient)
+  const doctor = await Doctor.findById(appointment.doctor);
+
+
+  const pMessage = `Your appointment with doctor ${doctor.name} has been rescheduled to be at ${req.body.dateOfAppointment}`
+  const dMessage = `Your appointment with patient ${patient.name} has been rescheduled to be at ${req.body.dateOfAppointment}`
+
+  const newNotification = await Notifications.create({ patient: req.body.patient, doctor: req.body.doctor, appoinmentDate: req.body.dateOfAppointment, appointmentStatus: appointment.status, patientMessage: pMessage, doctorMessage: dMessage });
+
+  doctor.notifications.push(newNotification);
+  await doctor.save();
+  patient.notifications.push(newNotification);
+  await patient.save();
+
+  await patient.save({ validateBeforeSave: false });
+  await doctor.save({ validateBeforeSave: false });
+
+  try {
+
+    await sendEmail({
+      email: patient.email,
+      subject: 'You Have New Notification!',
+      message: pMessage,
+    });
+
+    await sendEmail({
+      email: doctor.email,
+      subject: 'You Have New Notification!',
+      message: dMessage,
+    });
+
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        appointment,
+      },
+    });
+
+  }
+
+  catch (err) {
+    console.log(err)
+  }
+
 });
 
 const isDateInFuture = (dateToCompare) => {
