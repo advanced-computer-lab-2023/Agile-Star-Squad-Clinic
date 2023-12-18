df655760257d6248c7964a9ec0a76484d5ae22fc
Youssef Mostafa <88552711+Youssef-Hossam-Mostafa@users.noreply.github.com>
2023-12-18 18:46:22 +0200

    feat: add dropDown for medicineName

--- frontend/src/prescriptions/AddNewPrescription.js
+++ frontend/src/prescriptions/AddNewPrescription.js
@@ -1,47 +1,68 @@
-import ReactDOM from "react-dom";
-import React, { useState } from "react";
-import Modal from "../shared/components/Modal/Modal";
+import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from 'react';
+import Modal from '../shared/components/Modal/Modal';
+import axios from 'axios';
 
 const AddNewPrescription = (props) => {
-    const [addedBody,setAddedBody]=useState('')
-    const [items, setItems] = useState([]);
+  const [addedBody, setAddedBody] = useState('');
+  const [items, setItems] = useState([]);
   const [itemName, setItemName] = useState('');
   const [itemDosage, setItemDosage] = useState('');
   const [itemFrequency, setItemFrequency] = useState('');
-  const[showAddItemInputs,setShowAddItemInputs]= useState(false);
+  const [showAddItemInputs, setShowAddItemInputs] = useState(false);
+  const [medicineList, setMedicineList] = useState([]);
+  const [medicine, setMedicine] = useState(null);
+
+  const fetchMedicineList = async () => {
+    const response = await axios.get('http://localhost:4000/medicine');
+    console.log(response.data.data.medicines);
+    setMedicineList(response.data.data.medicines);
+    setMedicine(response.data.data.medicines[0]);
+  };
+
+  useEffect(() => {
+    fetchMedicineList();
+  }, []);
+
   const onDelete = async (event) => {
     event.preventDefault();
-    const toAddPrescription={doctor:props.doctor.userId,patient:props.patient,body:addedBody,items:items}
-    console.log("Presc",toAddPrescription)
-    
+    const toAddPrescription = {
+      doctor: props.doctor.userId,
+      patient: props.patient,
+      body: addedBody,
+      items: items,
+    };
+    console.log('Presc', toAddPrescription);
+
     try {
-        const response = await fetch('http://localhost:3000/prescriptions', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(toAddPrescription),
-        });
-  
-        if (response.ok) {
-          alert('Prescription Added Successfully');
-          props.exit();
-         props.onAddPrescription();
-        } else {
-          alert('Failed to send data.');
-        }
-      } catch (error) {
-        alert('Network error: ' + error.message);
+      const response = await fetch('http://localhost:3000/prescriptions', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(toAddPrescription),
+      });
+
+      if (response.ok) {
+        alert('Prescription Added Successfully');
+        props.exit();
+        props.onAddPrescription();
+      } else {
+        alert('Failed to send data.');
       }
-    
-    
+    } catch (error) {
+      alert('Network error: ' + error.message);
+    }
   };
   const addPresHandler = (e) => {
     // setaddNewOn(true);
     e.preventDefault();
     const newItem = {
-      name: itemName,
+      medicineId: medicine._id,
+      name: medicine.name,
       dosage: itemDosage,
-      frequency: itemFrequency
+      frequency: itemFrequency,
     };
+    alert('Medicine Added Successfully');
+    console.log(newItem);
     setItems([...items, newItem]);
     setItemName('');
     setItemDosage('');
@@ -53,42 +74,72 @@ const AddNewPrescription = (props) => {
     return date.toDateString(); // Adjust this to fit your desired date format
   };
   const getPrescBody = () => {
-    
     return (
       <React.Fragment>
-       
         <form onSubmit={addPresHandler}>
-        <div>
-          <label>Details</label>
-          <input type="text" value={addedBody} onChange={(e) => setAddedBody(e.target.value)} required></input>
-        </div>
-        
-        <div>
+          <div>
+            <label>Details</label>
+            <input
+              type="text"
+              value={addedBody}
+              onChange={(e) => setAddedBody(e.target.value)}
+              required
+            ></input>
+          </div>
+
+          <div>
             <h4>Prescribed Medications: </h4>
             {items.map((item, index) => (
               <div key={index}>
-                <p>Name : {item.name} &nbsp; Dosage : {item.dosage} &nbsp; Frequency : {item.frequency} </p>
-
+                <p>
+                  Name : {item.name} &nbsp; Dosage : {item.dosage} &nbsp;
+                  Frequency : {item.frequency}{' '}
+                </p>
               </div>
             ))}
           </div>
-          {!showAddItemInputs && <button onClick={()=>setShowAddItemInputs(true)}>Add New Item</button>}
-          {showAddItemInputs&&<div>
-            <h4>Add Medication:</h4>
+          {!showAddItemInputs && (
+            <button onClick={() => setShowAddItemInputs(true)}>
+              Add New Item
+            </button>
+          )}
+          {showAddItemInputs && medicineList && (
             <div>
-              <label>Name</label>
-              <input type="text" value={itemName} onChange={(e) => setItemName(e.target.value)} />
+              <h4>Add Medication:</h4>
+              <div>
+                <label>Name</label>
+                <select
+                  value={medicine.name}
+                  onChange={(e) => {
+                    setMedicine(e.target.value);
+                  }}
+                >
+                  {medicineList.map((medicine, index) => (
+                    <option key={index} value={medicine._id}>
+                      {medicine.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <label>Dosage</label>
+                <input
+                  type="text"
+                  value={itemDosage}
+                  onChange={(e) => setItemDosage(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Frequency</label>
+                <input
+                  type="text"
+                  value={itemFrequency}
+                  onChange={(e) => setItemFrequency(e.target.value)}
+                />
+              </div>
+              <button type="submit">Add</button>
             </div>
-            <div>
-              <label>Dosage</label>
-              <input type="text" value={itemDosage} onChange={(e) => setItemDosage(e.target.value)} />
-            </div>
-            <div>
-              <label>Frequency</label>
-              <input type="text" value={itemFrequency} onChange={(e) => setItemFrequency(e.target.value)} />
-            </div>
-            <button type="submit" >Add</button>
-          </div>}
+          )}
         </form>
       </React.Fragment>
     );
@@ -99,7 +150,7 @@ const AddNewPrescription = (props) => {
       {getPrescBody()}
       <ActionButtons onDelete={onDelete} />
     </Modal>,
-    document.getElementById("backdrop-root")
+    document.getElementById('backdrop-root'),
   );
 };
 
